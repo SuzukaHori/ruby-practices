175e2c896e4e0746ef32dc83fbc4ff550fe5fe8f
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-01 10:10:44 +0900

    変数名の変更・レイアウトのバグ修正
    変数名を直した。5月のカレンダーのレイアウトが崩れていたので修正した。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -28,16 +28,17 @@ set_year_and_month(params[:y], params[:m])
 puts "#{@month}月 #{@year}".center(19)
 puts "日 月 火 水 木 金 土"
 
-enum = Enumerator.new do|y| 
+enumerate_dates_of_month = Enumerator.new do|y| 
   (Date.new(@year, @month, 1)..Date.new(@year, @month, -1)).each{|i| y << i} #1日から月末日までの日付が入ったEmumeratorクラスを作る
 end
 
-enum.each do |date| #dateに日付を代入する
+enumerate_dates_of_month.each do |date| #dateに日付を代入する
   date.cwday.times {print "   "} if date.day == 1 && date.cwday!= 7 #1日かつ日曜日以外だった場合、曜日の数だけ空白を入れる  
-  print " " if date.day < 10 
   if  date.day == @day
-    print "\e[30m\e[47m#{date.day}\e[0m" #今日があった場合、色を反転させる
+    print "\e[30m\e[47m \e[0m" if date.day < 10
+    print "\e[30m\e[47m#{date.day}\e[0m " #今日があった場合、色を反転させる
   else
+    print " " if date.day < 10 
     print "#{date.day} "
   end
   print "\n" if date.saturday?  
