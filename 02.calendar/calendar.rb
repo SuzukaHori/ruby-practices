f65e6b7be8d24a58ab9f3577fb0dfb1844945136
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-01 15:34:43 +0900

    スペースの訂正、「''」に変更
    半角スペースのミスを修正した。一部を「""」から「''」に変更した。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,8 +5,8 @@ require 'optparse'
 
 opt = OptionParser.new
 params = {}
-opt.on('-y [VAL]') {|v| params[:y] = v.to_i } #オプション「-y」「-m」で指定された数をparamsに取得する
-opt.on('-m [VAL]') {|v| params[:m] = v.to_i }
+opt.on('-y [VAL]') { |v| params[:y] = v.to_i } # オプション「-y」「-m」で指定された数をparamsに取得する
+opt.on('-m [VAL]') { |v| params[:m] = v.to_i }
 opt.parse!(ARGV)
 
 def set_year_and_month(year, month)
@@ -28,17 +28,17 @@ set_year_and_month(params[:y], params[:m])
 puts "#{@month}月 #{@year}".center(19)
 puts "日 月 火 水 木 金 土"
 
-enumerate_dates_of_month = Enumerator.new do|y| 
+enumerate_dates_of_month = Enumerator.new do |y| 
   (Date.new(@year, @month, 1)..Date.new(@year, @month, -1)).each{|i| y << i} #1日から月末日までの日付が入ったEmumeratorクラスを作る
 end
 
-enumerate_dates_of_month.each do |date| #dateに日付を代入する
-  date.cwday.times {print "   "} if date.day == 1 && date.cwday!= 7 #1日かつ日曜日以外だった場合、曜日の数だけ空白を入れる  
-  if  date.day == @day
+enumerate_dates_of_month.each do |date| # dateに日付を代入する
+  date.cwday.times { print '   ' } if date.day == 1 && date.cwday!= 7 # 1日かつ日曜日以外だった場合、曜日の数だけ空白を入れる  
+  if date.day == @day
     print "\e[30m\e[47m \e[0m" if date.day < 10
-    print "\e[30m\e[47m#{date.day}\e[0m " #今日があった場合、色を反転させる
+    print "\e[30m\e[47m#{date.day}\e[0m " # 今日があった場合、色を反転させる
   else
-    print " " if date.day < 10 
+    print ' ' if date.day < 10 
     print "#{date.day} "
   end
   print "\n" if date.saturday?  
