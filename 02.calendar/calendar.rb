94476043eaf3d74845b1809fff51c735b6c6962e
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-02 20:09:06 +0900

    メソッドを削除し、変数を修正
    メソッドを定義する必要性がないと思ったので修正した。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -9,34 +9,30 @@ opt.on('-y [VAL]') { |v| params[:y] = v.to_i } # オプション「-y」「-m」
 opt.on('-m [VAL]') { |v| params[:m] = v.to_i }
 opt.parse!(ARGV)
 
-def set_year_and_month(year, month)
-  if year != nil
-    @year = year
-  else
-    @year = Date.today.year
-  end
-  if month != nil
-    @month = month
-  else
-    @month = Date.today.month
-  end
-  @day = Date.today.day if @year == Date.today.year && @month == Date.today.month
-end  
+if params[:y] == nil #オプションで指定された場合の値、もしくは今日の値を代入する
+  year = Date.today.year
+else
+  year = params[:y]
+end
+if params[:m] == nil
+  month = Date.today.month
+else
+  month = params[:m]
+end
+day = Date.today.day if year == Date.today.year && month == Date.today.month
 
-set_year_and_month(params[:y], params[:m])
-
-puts "#{@month}月 #{@year}".center(19)
+puts "#{month}月 #{year}".center(19)
 puts '日 月 火 水 木 金 土'
 
-enumerate_dates_of_month = Enumerator.new do |y| 
-  (Date.new(@year, @month, 1)..Date.new(@year, @month, -1)).each { |i| y << i } #1日から月末日までの日付が入ったEmumeratorクラスを作る
+dates = Enumerator.new do |y| 
+  (Date.new(year, month, 1)..Date.new(year, month, -1)).each { |i| y << i } #1日から月末日までの日付が入ったEmumeratorクラスを作る
 end
 
-enumerate_dates_of_month.each do |date| # dateに日付を代入する
+dates.each do |date| 
   date.cwday.times { print '   ' } if date.day == 1 && date.cwday!= 7 # 1日かつ日曜日以外だった場合、曜日の数だけ空白を入れる  
-  if date.day == @day
-    print "\e[30m\e[47m \e[0m" if date.day < 10
-    print "\e[30m\e[47m#{date.day}\e[0m " # 今日があった場合、色を反転させる
+  if date.day == day
+    print "\e[30m\e[47m \e[0m" if date.day < 10  # 今日があった場合、色を反転させる
+    print "\e[30m\e[47m#{date.day}\e[0m "
   else
     print ' ' if date.day < 10 
     print "#{date.day} "
