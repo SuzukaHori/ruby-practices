6e695e37f67dfeae7fb311182696b1eb1c73a0b9
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-07 11:02:25 +0900

    右寄せの方法の変更、コメントの削除、改行の変更
    rjustを使う方法に変更した。不要なコメントを削除した。フォーマット機能で改行等を変更した。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,11 +5,11 @@ require 'optparse'
 
 opt = OptionParser.new
 params = {}
-opt.on('-y [VAL]') { |v| params[:y] = v.to_i } # オプション「-y」「-m」で指定された数をparamsに取得する
-opt.on('-m [VAL]') { |v| params[:m] = v.to_i }
+opt.on("-y [VAL]") { |v| params[:y] = v.to_i }
+opt.on("-m [VAL]") { |v| params[:m] = v.to_i }
 opt.parse!(ARGV)
 
-if params[:y] == nil #オプションで指定された場合の値、もしくは今日の値を代入する
+if params[:y] == nil
   year = Date.today.year
 else
   year = params[:y]
@@ -24,20 +24,21 @@ day = Date.today.day if year == Date.today.year && month == Date.today.month
 puts "#{month}月 #{year}".center(19)
 puts '日 月 火 水 木 金 土'
 
-dates = Enumerator.new do |y| 
-  (Date.new(year, month, 1)..Date.new(year, month, -1)).each { |i| y << i } #1日から月末日までの日付が入ったEmumeratorクラスを作る
-end
+dates =
+  Enumerator.new do |y|
+    (Date.new(year, month, 1)..Date.new(year, month, -1)).each { |i| y << i }
+  end
 
-dates.each do |date| 
-  date.cwday.times { print '   ' } if date.day == 1 && date.cwday!= 7 # 1日かつ日曜日以外だった場合、曜日の数だけ空白を入れる  
+dates.each do |date|
+  date.cwday.times { print "   " } if date.day == 1 && date.cwday != 7
   if date.day == day
-    print "\e[30m\e[47m \e[0m" if date.day < 10  # 今日があった場合、色を反転させる
+    print "\e[30m\e[47m \e[0m" if date.day < 10
     print "\e[30m\e[47m#{date.day}\e[0m "
   else
-    print ' ' if date.day < 10 
-    print "#{date.day} "
+    print "#{date.day}".rjust(2, " ")
+    print " "
   end
-  print "\n" if date.saturday?  
+  print "\n" if date.saturday?
 end
 
 puts "\n"
