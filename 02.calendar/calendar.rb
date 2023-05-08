500a4ae1e77d7e048f3cb8aae67e62fa6d020d5a
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-08 13:05:20 +0900

    文字の反転の修正
    エスケープコードを修正し、効果が継続する部分のコードを削除しました。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'date'
-require 'optparse'
+require "date"
+require "optparse"
 
 opt = OptionParser.new
 params = {}
@@ -22,13 +22,14 @@ end
 day = Date.today.day if year == Date.today.year && month == Date.today.month
 
 puts "#{month}月 #{year}".center(19)
-puts '日 月 火 水 木 金 土'
+puts "日 月 火 水 木 金 土"
 
 (Date.new(year, month, 1)..Date.new(year, month, -1)).each do |date|
   date.cwday.times { print "   " } if date.day == 1 && date.cwday != 7
   if date.day == day
-    print "\e[30m\e[47m \e[0m" if date.day < 10
-    print "\e[30m\e[47m#{date.day}\e[0m "
+    print "\e[7m " if date.day < 10
+    print "#{date.day}\e[0m"
+    print " "
   else
     print "#{date.day}".rjust(2, " ")
     print " "
