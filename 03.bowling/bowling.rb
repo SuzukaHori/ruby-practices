18ea475f35b18437c5a5755a94d16369ceeb8f97
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-08 21:28:22 +0900

    ストライクのメソッドを追加
    条件分岐が複雑になったため、ストライクのメソッドを追加しました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -16,24 +16,29 @@ end
 frames = shots.each_slice(2).to_a
 frames[9].push(*frames.pop) while frames[10]
 
-point = 0
 STRIKE_POINT = 10
+
+def calculate_strike(frames, index, next_frame)
+  return frames[9].sum if index == 9
+
+  if next_frame[0] == STRIKE_POINT
+    if index == 8
+      STRIKE_POINT * 2 + next_frame[2]
+    else
+      STRIKE_POINT * 2 + frames[index + 2][0]
+    end
+  else
+    STRIKE_POINT + next_frame[0] + next_frame[1]
+  end
+end
+
+point = 0
 
 frames.each_with_index do |frame, index|
   next_frame = frames[index + 1]
   point +=
-    if index == 9
-      frame.sum
-    elsif frame[0] == STRIKE_POINT
-      if next_frame[0] == STRIKE_POINT
-        if index == 8
-          STRIKE_POINT * 2 + next_frame[2]
-        else
-          STRIKE_POINT * 2 + frames[index + 2][0]
-        end
-      else
-        STRIKE_POINT + next_frame[0] + next_frame[1]
-      end
+    if frame[0] == STRIKE_POINT
+      calculate_strike(frames, index, next_frame)
     elsif frame.sum == STRIKE_POINT
       STRIKE_POINT + next_frame[0]
     else
