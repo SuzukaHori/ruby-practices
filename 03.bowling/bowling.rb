904682e41547fefd747bf70adf53c332ba2c8f0c
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-09 16:51:11 +0900

    メソッドの引数indexを削除
    変数next_next_frameを追加し、引数のindexを削除しました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,10 +2,10 @@
 # frozen_string_literal: true
 
 score = ARGV[0]
-scores = score.split(",")
+scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == "X"
+  if s == 'X'
     shots << 10
     shots << 0
   else
@@ -18,14 +18,14 @@ frames[9].push(*frames.pop) while frames[10]
 
 STRIKE_POINT = 10
 
-def calculate_strike(frames, index, next_frame)
-  return frames[9].sum if index == 9
+def calculate_strike(frames, next_frame, next_next_frame)
+  return frames[9].sum if next_frame.nil?
 
   if next_frame[0] == STRIKE_POINT
-    if index == 8
+    if next_next_frame.nil?
       STRIKE_POINT * 2 + next_frame[2]
     else
-      STRIKE_POINT * 2 + frames[index + 2][0]
+      STRIKE_POINT * 2 + next_next_frame[0]
     end
   else
     STRIKE_POINT + next_frame[0] + next_frame[1]
@@ -36,9 +36,10 @@ point = 0
 
 frames.each_with_index do |frame, index|
   next_frame = frames[index + 1]
+  next_next_frame = frames[index + 2]
   point +=
     if frame[0] == STRIKE_POINT
-      calculate_strike(frames, index, next_frame)
+      calculate_strike(frames, next_frame, next_next_frame)
     elsif frame.sum == STRIKE_POINT
       STRIKE_POINT + next_frame[0]
     else
