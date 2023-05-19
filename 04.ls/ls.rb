266cdc6dc7e21754cc29458efb6963ebe093b4e7
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-05-19 12:03:20 +0900

    ls.rbを作成

---
+++ 04.ls/ls.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+files = Dir.glob('*')
+
+def calculate_number_of_rows(files)
+  number_of_columns = 3
+  (files.size / number_of_columns.to_f).ceil
+end
+
+def format_files(files, number_of_rows)
+  column_spacing = files.map(&:size).max + 1
+  files
+    .map { |file| file.ljust(column_spacing) }
+    .each_slice(number_of_rows)
+    .to_a
+    .each { |file| file << nil while file.size < number_of_rows }
+    .transpose
+end
+
+number_of_rows = calculate_number_of_rows(files)
+unless files.empty?
+  formatted_files = format_files(files, number_of_rows)
+  formatted_files.each { |file| print(*file, "\n") }
+end
