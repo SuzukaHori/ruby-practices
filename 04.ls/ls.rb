957a1de1bdb629438e7f8122890bcc733ba91c85
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-06-17 12:19:30 +0900

    -lオプションを追加
    -lオプションでファイルの情報が表示されるようにしました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,16 +2,18 @@
 # frozen_string_literal: true
 
 require 'optparse'
-
-opt = OptionParser.new
-options = {}
-opt.on('-r')
-opt.parse!(ARGV, into: options)
-
-files = Dir.glob('*', base: ARGV.join)
-files = files.reverse if options[:r]
+require 'etc'
 
 NUMBER_OF_COLUMNS = 3
+INFO_KEYS = %i[
+  type_and_permission
+  hard_link_count
+  user_name
+  group_name
+  timestamp
+  size
+  file_name
+].freeze
 
 def calculate_number_of_rows(files)
   (files.size / NUMBER_OF_COLUMNS.to_f).ceil
@@ -26,8 +28,102 @@ def format_files(files, number_of_rows)
     .transpose
 end
 
-number_of_rows = calculate_number_of_rows(files)
-unless files.empty?
-  formatted_files = format_files(files, number_of_rows)
-  formatted_files.each { |file| print(*file, "\n") }
+def build_file_info(file_name, file_status)
+  file_mode = file_status.mode
+  { type_and_permission:
+      get_file_type(file_mode) + generate_permission_string(file_mode),
+    hard_link_count: file_status.nlink.to_s,
+    user_name: get_user_name(file_status),
+    group_name: Etc.getgrgid(file_status.gid).name,
+    size: file_status.size.to_s,
+    timestamp: file_status.mtime.strftime('%_m %e %H:%M'),
+    file_name: }
+end
+
+def get_user_name(file_status)
+  Etc.getpwuid(file_status.uid).name
+rescue ArgumentError
+  file_status.uid.to_s
+end
+
+def generate_permission_string(file_mode)
+  permission_list = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-',
+                      '7' => 'rwx' }
+  permissions =
+    file_mode
+    .to_s(8)
+    .slice(-3, 3)
+    .chars
+    .map { |permission_number| permission_list[permission_number] }
+  permissions.join
+end
+
+def get_file_type(mode)
+  type_list = {
+    '01' => 'p',
+    '02' => 'c',
+    '04' => 'd',
+    '06' => 'b',
+    '10' => '-',
+    '12' => 'l',
+    '14' => 's'
+  }
+  type_list[mode.to_s(8).rjust(6, '0')[0, 2]]
+end
+
+def align_file_infos(file_infos, info_lengths)
+  file_infos.each do |file_info|
+    INFO_KEYS.each do |key|
+      max_size = info_lengths[key].max
+      file_info[key] = if %i[user_name group_name file_name].include?(key)
+                         file_info[key].ljust(max_size)
+                       else
+                         file_info[key].rjust(max_size)
+                       end
+    end
+  end
+end
+
+def process_l_option(file_names)
+  file_infos = []
+  file_blocks = []
+  info_lengths = Hash.new { |hash, key| hash[key] = [] }
+
+  file_names.each do |file_name|
+    file_status = File.stat(file_name)
+    current_file_info = build_file_info(file_name, file_status)
+    file_infos << current_file_info
+    store_totals_and_length(info_lengths, current_file_info, file_blocks, file_status)
+  end
+  [file_blocks.sum, align_file_infos(file_infos, info_lengths)]
+end
+
+def store_totals_and_length(info_lengths, current_file_info, file_blocks, file_status)
+  INFO_KEYS.each { |key| info_lengths[key] << current_file_info[key].length }
+  file_blocks << file_status.blocks
+end
+
+opt = OptionParser.new
+options = {}
+opt.on('-l')
+opt.parse!(ARGV, into: options)
+
+files = Dir.glob('*', base: ARGV.join)
+
+if options[:l]
+  total_file_blocks, formatted_file_infos = process_l_option(files)
+  puts "total #{total_file_blocks}"
+  formatted_file_infos.each do |file_info|
+    file_info.each do |key, value|
+      print ' ' if %i[hard_link_count group_name size].include?(key)
+      print "#{value} "
+    end
+    puts
+  end
+else
+  number_of_rows = calculate_number_of_rows(files)
+  unless files.empty?
+    formatted_files = format_files(files, number_of_rows)
+    formatted_files.each { |file| print(*file, "\n") }
+  end
 end
