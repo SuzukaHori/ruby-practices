91b727afcb10858ff6888b8fee1ad1580b5f0150
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-09-28 11:27:30 +0900

    mainのテストケースを修正

--- 06.bowling_object/test/main_test.rb
+++ 06.bowling_object/test/main_test.rb
@@ -14,21 +14,39 @@ class MainTest < Test::Unit::TestCase
     $stdout = STDOUT
   end
 
-  test 'ケース1' do
+  test 'スペアやストライクがある' do
     setup_argv_and_output(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'])
     main
-    assert_equal 139, $stdout.string.chomp.to_i
+    assert_equal "139\n", $stdout.string
   end
 
-  test 'ケース2' do
+  test '最終フレームで3投目を投げる' do
+    setup_argv_and_output(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'])
+    main
+    assert_equal "144\n", $stdout.string
+  end
+
+  test '最終フレームが連続ストライク' do
     setup_argv_and_output(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'])
     main
-    assert_equal 164, $stdout.string.chomp.to_i
+    assert_equal "164\n", $stdout.string
   end
 
-  test 'ケース3' do
+  test '全てストライク' do
     setup_argv_and_output(['X,X,X,X,X,X,X,X,X,X,X,X'])
     main
-    assert_equal 300, $stdout.string.chomp.to_i
+    assert_equal "300\n", $stdout.string
+  end
+
+  test '最終フレーム以外で連続ストライク' do
+    setup_argv_and_output(['6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'])
+    main
+    assert_equal "134\n", $stdout.string
+  end
+
+  test '0とストライクのみ' do
+    setup_argv_and_output(['X,0,0,X,0,0,X,0,0,X,0,0,X,0,0'])
+    main
+    assert_equal "50\n", $stdout.string
   end
 end
