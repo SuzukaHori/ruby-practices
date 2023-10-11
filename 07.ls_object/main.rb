7312d0a7c3bfe2c74d5823be012b39dee70549ea
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-10-11 22:17:52 +0900

    -rオプションを追加

--- 07.ls_object/main.rb
+++ 07.ls_object/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 class FileInfo
   attr_reader :name
 
@@ -14,8 +15,16 @@ class List
 
   NUMBER_OF_COLUMNS = 3
 
-  def initialize(file_names)
+  def initialize
+    file_names = Dir.glob('*')
     @files = create_formatted_files(file_names)
+  end
+
+  def display
+    files.each do |array|
+      array.each { |file| print file&.name }
+      print "\n"
+    end
   end
 
   private
@@ -34,10 +43,26 @@ class List
   end
 end
 
-file_names = Dir.glob('*', base: ARGV.join)
-list = List.new(file_names)
-
-list.files.each do |array|
-  array.each { |file| print file&.name }
-  print "\n"
+class ReverseList < List
+  def format(files)
+    super(files.reverse)
+  end
 end
+
+def parse_options(argv)
+  opt = OptionParser.new
+  options = {}
+  opt.on('-r')
+  opt.parse!(argv, into: options)
+  options
+end
+
+options = parse_options(ARGV)
+
+list = if options[:r]
+         ReverseList.new
+       else
+         List.new
+       end
+
+list.display
