312efac87297fdbc3648fcf736cbbce475cb3464
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-10-14 16:14:06 +0900

    Permissionクラスを追加

--- 07.ls_object/file_info.rb
+++ 07.ls_object/file_info.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'etc'
+require_relative './permission'
 
 class FileInfo
   attr_reader :name, :details, :blocks
@@ -22,7 +23,7 @@ class FileInfo
   def set_details
     status = File.stat(name)
     @details =
-      { type_and_permission: generate_type_and_permission(status),
+      { type_and_permission: Permission.new(status).type_and_permission,
         hard_link_count: status.nlink.to_s,
         user_name: Etc.getpwuid(status.uid).name,
         group_name: Etc.getgrgid(status.gid).name,
@@ -50,40 +51,4 @@ class FileInfo
       details[sym].rjust(spacing)
     end
   end
-
-  private
-
-  def generate_type_and_permission(status)
-    permissions =
-      status.mode
-            .to_s(8)
-            .slice(-3, 3)
-            .chars
-            .map { |n| PERMISSION_LIST[n] }
-            .join
-    TYPE_LIST[format('%06o', status.mode)[0, 2]] + permissions
-  end
-
-  TYPE_LIST = {
-    '01' => 'p',
-    '02' => 'c',
-    '04' => 'd',
-    '06' => 'b',
-    '10' => '-',
-    '12' => 'l',
-    '14' => 's'
-  }.freeze
-
-  PERMISSION_LIST = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }.freeze
-
-  private_constant :TYPE_LIST, :PERMISSION_LIST
 end
